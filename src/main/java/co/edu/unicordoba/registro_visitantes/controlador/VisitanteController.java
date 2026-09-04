5c7302428ad1b57a929453a402bdb41809acda54
josedario2323-svg <josedario2323@gmail.com>
2026-09-04 18:31:15 -0500

    se actualiza el post del controller

--- src/main/java/co/edu/unicordoba/registro_visitantes/controlador/VisitanteController.java
+++ src/main/java/co/edu/unicordoba/registro_visitantes/controlador/VisitanteController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,9 @@ public class VisitanteController {
     }
 
     @PostMapping
-    public Visitante registrar(@RequestParam String nombre, @RequestParam int edad){
+    public Visitante registrar(@RequestBody Map<String, Object> body) {
+        String nombre = (String) body.get("nombre");
+        int edad = Integer.parseInt(body.get("edad").toString());
         return servicio.registrar(nombre, edad);
     }
 
